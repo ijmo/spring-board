4f806a04cbf15e779535423cf3d0066d309bd6d8
ijmo <jinmo.im@gmail.com>
2019-06-18 18:41:30 +0900

    Add onclick handlers

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,61 @@
+
+function refreshComments(arr) {
+    $(".comments").empty();
+    arr.forEach(function(row) {
+        console.log();
+        $(".comments").append($("<div></div>")
+            .html($("<span></span>").text(row.message.body)));
+    });
+
+}
+
+function getComments(postPath) {
+    if ($(".comments").length > 0) {
+        $.ajax({
+            url: postPath + "/comments",
+            type: "GET",
+            success: function (data) {
+                console.log(data);
+                refreshComments(data);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.error("error: " + xhr.status);
+                console.error("error: " + xhr.responseText);
+                console.error(thrownError);
+            }
+        });
+    }
+}
+
 $(document).ready(function() {
     console.log("Welcome to Spring Board Demo!");
-    var commentRow = $("<div></div>");
-    // commentRow.text("123");
-    // $(".comments").append(commentRow);
+
+    var path = window.location.pathname;
+    getComments(path);
+
+    $("#post-comment-btn").click(function() {
+        console.log();
+        var commentText = $("#comment-text");
+        console.log(commentText.val());
+        $.ajax({
+            url: path + "/comments/new",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({body: commentText.val()}),
+            success: function () {
+                console.log("done");
+                commentText.val("");
+                getComments(path);
+            },
+            error: function () {
+                console.error("error: " + xhr.status);
+                console.error("error: " + xhr.responseText);
+                console.error(thrownError);
+            }
+        });
+    });
+
+    $(".posts-table tbody tr").click(function() {
+
+    });
 });
