c390c8133089c71e14f8bcdb88293b8ef094d6a0
ijmo <jinmo.im@gmail.com>
2019-06-20 12:54:46 +0900

    Add test for UserService

--- src/test/java/ijmo/demo/springboard/user/UserServiceTest.java
+++ src/test/java/ijmo/demo/springboard/user/UserServiceTest.java
@@ -1,4 +1,36 @@
 package ijmo.demo.springboard.user;
 
-public class UserServiceTest {
+import ijmo.demo.springboard.test.BaseTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class UserServiceTest extends BaseTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void addUserTest() {
+        final String USERNAME1 = "test1";
+
+        userService.loginOrSignUp(USERNAME1);
+        Optional<User> optionalUser = userRepository.findByUsername(USERNAME1);
+
+        softAssertions.assertThat(optionalUser.orElse(null)).isNotNull();
+
+        softAssertions.assertThat(optionalUser.get().getUsername())
+                .isEqualTo(USERNAME1);
+    }
 }
