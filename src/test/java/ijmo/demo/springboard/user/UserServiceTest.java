8afdbceb5e323af6484f57066267a38cf99add04
ijmo <jinmo.im@gmail.com>
2019-06-20 23:26:24 +0900

    Remove @ActiveProfiles and Change test name

--- src/test/java/ijmo/demo/springboard/user/UserServiceTest.java
+++ src/test/java/ijmo/demo/springboard/user/UserServiceTest.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
-@ActiveProfiles("local")
 public class UserServiceTest extends BaseTest {
 
     @Autowired
@@ -24,7 +22,7 @@ public class UserServiceTest extends BaseTest {
     private UserService userService;
 
     @Test
-    public void User_Add() {
+    public void whenAddUser_thenSavedToRepository() {
         final String USERNAME1 = "test1";
 
         userService.loginOrSignUp(USERNAME1);
