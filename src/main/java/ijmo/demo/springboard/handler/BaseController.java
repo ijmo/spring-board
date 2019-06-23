55098d7e0116b866dcc9f41c32e1f6af7c1f350d
ijmo <jinmo.im@gmail.com>
2019-06-23 20:57:53 +0900

    Add key for user session as final

--- src/main/java/ijmo/demo/springboard/handler/BaseController.java
+++ src/main/java/ijmo/demo/springboard/handler/BaseController.java
@@ -1,15 +1,16 @@
 package ijmo.demo.springboard.handler;
 
+import ijmo.demo.springboard.session.SessionUtils;
 import ijmo.demo.springboard.session.UserSession;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-@SessionAttributes("userSession")
+@SessionAttributes(SessionUtils.USER_SESSION_KEY)
 @Validated
 public class BaseController {
 
-    @ModelAttribute("userSession")
+    @ModelAttribute(SessionUtils.USER_SESSION_KEY)
     public UserSession userSession() {
         return new UserSession();
     }
--- src/main/java/ijmo/demo/springboard/session/SessionUtils.java
+++ src/main/java/ijmo/demo/springboard/session/SessionUtils.java
@@ -1,8 +1,20 @@
 package ijmo.demo.springboard.session;
 
+import ijmo.demo.springboard.user.User;
+
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 public class SessionUtils {
+    public static final String USER_SESSION_KEY = "userSession";
+
+    public static Optional<User> getUserFrom(HttpSession session) {
+        UserSession userSession = (UserSession) session.getAttribute(SessionUtils.USER_SESSION_KEY);
+        if (userSession != null) {
+            return userSession.getUser();
+        }
+        return Optional.empty();
+    }
     public static void clear(HttpSession session) {
         session.invalidate();
     }
