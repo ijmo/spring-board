952b53d9640756822a0e8772a72a15c7a5aa7574
ijmo <jinmo.im@gmail.com>
2019-06-24 15:53:52 +0900

    Add readOnly for @Transactional

--- src/main/java/ijmo/demo/springboard/message/CommentRepository.java
+++ src/main/java/ijmo/demo/springboard/message/CommentRepository.java
@@ -1,9 +1,11 @@
 package ijmo.demo.springboard.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Transactional(readOnly = true)
     List<Comment> findAllByPostIdAndIsDeleted(long postId, boolean isDeleted);
 }
--- src/main/java/ijmo/demo/springboard/message/PostRepository.java
+++ src/main/java/ijmo/demo/springboard/message/PostRepository.java
@@ -1,11 +1,15 @@
 package ijmo.demo.springboard.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Transactional(readOnly = true)
     List<Post> findAllByIsDeletedOrderByCreatedOnDesc(boolean isDeleted);
+
+    @Transactional(readOnly = true)
     Optional<Post> findByIdAndIsDeleted(long postId, boolean isDeleted);
 }
