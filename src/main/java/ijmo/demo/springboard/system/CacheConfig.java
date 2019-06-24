45dc05594c9392e0ffb5e4372d8af78354a45578
ijmo <jinmo.im@gmail.com>
2019-06-24 21:46:07 +0900

    Add EhCacheManagerFactoryBean bean

---
+++ src/main/java/ijmo/demo/springboard/system/CacheConfig.java
@@ -0,0 +1,19 @@
+package ijmo.demo.springboard.system;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheCacheManager() {
+        EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        cacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        cacheManagerFactoryBean.setShared(true);
+        return cacheManagerFactoryBean;
+    }
+}
