4b52515d9875168d9e0d1b2a96c7dd7f4816af32
ijmo <jinmo.im@gmail.com>
2019-06-27 03:23:21 +0900

    Use bootstrap

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,64 +1,73 @@
 
+function getAjaxErrorHandler(msg) {
+    return function(jqXHR, textStatus, errorThrown) {
+        console.error("error while " + msg + " : " + jqXHR.status);
+        console.error("error while " + msg + " : " + jqXHR.responseText);
+        console.error(textStatus);
+        console.error(errorThrown);
+    }
+}
+
 function refreshComments(arr) {
-    $(".comments").empty();
+    var comments = $("#comments");
+    comments.empty();
     arr.forEach(function(row) {
-        $(".comments").append(
-            $("<div class='comment-row'></div>")
+        comments.append(
+            $("<div class='p-1 mb-2 comment-item'></div>")
                 .append(
-                    $("<div class='comment-row-header'></div>")
-                        .append($("<span class='comment-username'></span>").text(row.user.username))
-                        .append($("<span class='comment-datetime'></span>").text(moment(row.createdOn).format("YYYY-MM-DD HH:mm"))))
-                .append($("<div class='comment-view'></div>").append($("<span></span>").text(row.message.body))));
+                    $("<div class='d-flex flex-row text-light m-1 comment-header align-items-center'></div>")
+                        .append($("<span class='mr-auto pl-lg-3'></span>").text(row.user.username))
+                        .append($("<span class='date align-middle pr-lg-3'></span>").text(moment(row.createdOn).format("YYYY-MM-DD HH:mm"))))
+                .append($("<div class='mx-1 text-dark'></div>").append($("<span></span>").text(row.message.body))));
     });
 }
 
 function getComments(postPath) {
-    if ($(".comments").length > 0) {
+    if ($("#comments").length > 0) {
         $.ajax({
             url: postPath + "/comments",
             type: "GET",
-            success: function (data) {
-                // console.log(data);
+            success: function (data, textStatus, jqXHR) {
+                console.log(data);
+                console.log(jqXHR);
                 refreshComments(data);
             },
-            error: function (xhr, ajaxOptions, thrownError) {
-                console.error("error: " + xhr.status);
-                console.error("error: " + xhr.responseText);
-                console.error(thrownError);
-            }
+            error: getAjaxErrorHandler("getComments()")
         });
     }
 }
 
 $(document).ready(function() {
-    console.log("Welcome to Spring Board Demo!");
+    console.log("Welcome to Spring Board Demo!!");
 
     var path = window.location.pathname;
     getComments(path);
 
-    $("#post-comment-btn").click(function() {
-        console.log();
-        var commentText = $("#comment-text");
-        console.log(commentText.val());
-        $.ajax({
-            url: path + "/comments/new",
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify({body: commentText.val()}),
-            success: function () {
-                console.log("done");
-                commentText.val("");
-                getComments(path);
-            },
-            error: function () {
-                console.error("error: " + xhr.status);
-                console.error("error: " + xhr.responseText);
-                console.error(thrownError);
-            }
-        });
+    ///////////////////////////////////////////////////////
+    // Event handlers
+    ///////////////////////////////////////////////////////
+
+    // post list table row
+    $("#posts tbody tr").click(function() {
+        window.location = $(this).data("href");
     });
 
-    $(".posts-table tbody tr").click(function() {
-
+    // button for new comment in postDetails.html
+    $("#form-comment").submit(function() {
+        var messageBody = $("#form-comment #body");
+        $.ajax({
+            url: path + "/comments/new",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify({body: messageBody.val()}),
+            success: function (data, textStatus, jqXHR) {
+                // console.log(data);
+                // console.log(jqXHR);
+                messageBody.val("");
+                getComments(path);
+            },
+            error: getAjaxErrorHandler("#form-comment.submit")
+        });
+        return false;
     });
 });
