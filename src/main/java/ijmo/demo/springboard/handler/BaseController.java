3a8d8f0e108840629d2d447a3f2bb973df8e7034
ijmo <jinmo.im@gmail.com>
2019-06-27 01:52:36 +0900

    Remove @Validated

--- src/main/java/ijmo/demo/springboard/handler/BaseController.java
+++ src/main/java/ijmo/demo/springboard/handler/BaseController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @SessionAttributes(SessionUtils.USER_SESSION_KEY)
-@Validated
 public class BaseController {
 
     @ModelAttribute(SessionUtils.USER_SESSION_KEY)
